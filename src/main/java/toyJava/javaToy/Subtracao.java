d6bd246a1ceca13546fb4c56da67cd767c8752f8
leusonmario <leusonmario@gmail.com>
2016-07-01 16:37:54 -0300

    Add subtracao

---
+++ src/main/java/toyJava/javaToy/Subtracao.java
@@ -0,0 +1,11 @@
+package toyJava.javaToy;
+
+public class Subtracao {
+	private int numeroUm;
+	private int numeroDois;
+	
+	public Subtracao() {
+		this.numeroUm = 0;
+		this.numeroDois = 0;
+	}
+}
