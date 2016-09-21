df7d7007ea5def4c0ad0d8d9dd9903d5caac4aa7
leusonmario <leusonmario@gmail.com>
2016-09-21 14:12:07 -0300

    Testes paralelos - Travis

--- src/test/java/toyJava/javaToy/SomaTest.java
+++ src/test/java/toyJava/javaToy/SomaTest.java
@@ -1,16 +1,31 @@
 package toyJava.javaToy;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 public class SomaTest {
+	Soma soma = new Soma();
 
 	@Test
-	public void testSoma(){
-		Soma soma = new Soma();
+	public void testSomaUmParalelizando() {
 		soma.setNumeroUm(6);
+		System.out.println("Esse foi primeiro");
 		assertEquals("error in Soma", 6, soma.getSoma());
+	}
+	
+	@Test
+	public void testSomaDoinParalelizando() {
+		soma.setNumeroUm(7);
+		System.out.println("Esse foi segundo");
+		assertEquals("error in Soma", 7, soma.getSoma());
+	}
+	
+	@Test
+	public void testSomTresParalelizando() {
+		soma.setNumeroUm(8);
+		System.out.println("Esse foi terceiro");
+		assertEquals("error in Soma", 8, soma.getSoma());
 	}
 
 }
