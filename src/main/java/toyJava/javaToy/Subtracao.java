ae007ae4b7fbe18cc7e47907c4372aa361b78bf3
merge-conflict-factors <merge.conflict.factors@gmail.com>
2019-08-16 14:00:23 -0300

    changing pom

---
+++ src/main/java/toyJava/javaToy/Subtracao.java
@@ -0,0 +1,31 @@
+package toyJava.javaToy;
+
+public class Subtracao {
+	private int numeroUm;
+	private int numeroDois;
+	
+	public Subtracao() {
+		this.numeroUm = 0;
+		this.numeroDois = 0;
+	}
+	public int getNumeroUm(){
+		return this.numeroUm;
+	}
+	public int getNumeroDois(){
+		return this.numeroDois;
+	}
+	public getNumeroUm(){
+		return this.numeroUm;
+	}	
+	public int getSub(){
+		return this.numeroUm - this.numeroDois;
+	}
+
+	public void setNumeroUm(int novoNumero){
+		this.numeroUm = novoNumero;
+	}
+
+	public void setNumeroDois(int novoNumber){
+		this.numeroDois = novoNumber;
+	}
+}
---
+++ src/test/java/toyJava/javaToy/SubTest.java
@@ -0,0 +1,28 @@
+package toyJava.javaToy;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SubTest {
+	
+	@Test
+	public void testaSub(){
+		Subtracao sub = new Subtracao();
+		sub.setNumeroUm(1)
+		assertEquals(1, sub.getSub());
+	}
+	
+	@Test
+	public void testaSubSemSet(){
+		Subtracao sub = new Subtracao();
+		assertEquals(0, sub.getSub());
+	}
+	
+	@Test
+	public void testaSubComSet(){
+		Subtracao sub = new Subtracao();
+		sub.setNumeroUm(2);
+		assertEquals(2, sub.getSub());
+	}
+}
